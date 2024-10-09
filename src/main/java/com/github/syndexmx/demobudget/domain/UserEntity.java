c8627e1cc78b93cf51c592066e900ead144c0717
Maxim Shapovalov <syndex@list.ru>
2024-10-09 15:40:02 +0300

    User Repo

---
+++ src/main/java/com/github/syndexmx/demobudget/domain/UserEntity.java
@@ -0,0 +1,22 @@
+package com.github.syndexmx.demobudget.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    private String userName;
+
+    private String email;
+
+}
