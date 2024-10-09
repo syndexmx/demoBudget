789b21690eb97df6b403d802556d9a7956a2cc2c
Maxim Shapovalov <syndex@list.ru>
2024-10-09 15:09:37 +0300

    Initial Commit

---
+++ src/main/java/com/github/syndexmx/demobudget/DemobudgetApplication.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.demobudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemobudgetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/syndexmx/demobudget/DemobudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.syndexmx.demobudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
